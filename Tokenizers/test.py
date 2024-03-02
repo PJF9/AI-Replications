77762f4d39e1ae98b8488897a2a0a2dba77ab781
Peter Jacob <128411298+PJF9@users.noreply.github.com>
2024-03-02 22:23:45 +0200

    Update test.py

--- Tokenizers/test.py
+++ Tokenizers/test.py
@@ -1,1 +1,19 @@
+from Wordpiee import WordPiece
+from Unigram import Unigram
 
+
+corpus = []
+with open("Romeo and Juliet.txt") as f:
+    corpus = f.readlines()
+
+tokenizer = Unigram(corpus=corpus, ntokens=1000) ## Or WordPiece
+
+tokenizer.fit()
+
+print(tokenizer.tokenize("O, what a rash and bloody deed is this!"))
+
+text_encoding = tokenizer.encode("O, what a rash and bloody deed is this!")
+print(text_encoding)
+
+text_decoding = tokenizer.decode(text_encoding)
+print(text_decoding)
