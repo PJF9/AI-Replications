99fcfa11d062ee3b58cd22941564866930bef2a0
Peter Jacob <128411298+PJF9@users.noreply.github.com>
2024-03-03 11:19:46 +0200

    Update example.py

--- Transformer/example.py
+++ Transformer/example.py
@@ -1,20 +1,83 @@
-from Encoder import TransformerEncoder
+import torch
+from torch.nn import functional as F
 
 import pandas as pd
+from tqdm import tqdm
+
+from Encoder import TransformerEncoder
+
+
+
+### Creating the Function that Splits the Dataset into Batches
+def get_batch(split=None):
+    data = train_ds if split == "train" else valid_ds
+
+    idx_xb = torch.randint(len(data), (BATCH_SIZE,)) # Random indexes from the Dataset
+
+    x = torch.stack([torch.tensor(data["tokenized_text"][i.item()], dtype=torch.long) for i in idx_xb])
+    y = torch.tensor([data["label"][i.item()] for i in idx_xb], dtype=torch.long)    
+
+    return x.to(device=device, non_blocking=True), y.to(device=device, non_blocking=True)
+
+
+
+### Creating a Loss Calculation Function
+@torch.inference_mode()
+def estimate_loss(model):
+    out = {}
+
+    model.eval()
+    for split in ["train", "valid"]:
+        losses = torch.zeros(EVAL_ITERS)
+
+        # Iterate `EVAL_ITERS` number of times to reduce the noice that each batch contains
+        for i in range(EVAL_ITERS):
+            xb, yb = get_batch(split)
+            _, loss = model(xb, yb)
+            losses[i] = loss.item()
+        
+        out[split] = losses.mean()
+    
+    model.train()
+    return out
+
+
+
+### Creating the function that will generate predictions
+@torch.inference_mode()
+def predict(model, idx):
+    model.eval()
+
+    # Adding batch dimension for individual predictions
+    if idx.dim() == 1:
+        idx = idx.unsqueeze(dim=0)
+
+    logits, _ = model(idx)
+    probs = F.softmax(logits, dim=-1)
+
+    model.train()
+
+    return [{"Ham": probs[i][0][0].item(), "Spam": probs[i][0][1].item()} for i in range(idx.shape[0])]
+
+
 
 ### Hyperparameters
-BATCH_SIZE = 32
-EMBED_SIZE = 300
-EPOCHS = 500
-LR = 3e-4
-EVAL_INTERVAL = 50
-EVAL_ITERS = 5
-NUM_HEADS = 6
-NUM_LAYERS = 6
-DROPOUT = 0.2
-SCALE_EMBEDS = 4
+BATCH_SIZE = 32         # the size of sample (in our case character tokens) to create batches 
+EMBED_SIZE = 300        # the size of the embeddings for each token
+EPOCHS = 150            # the number of times we iterate all the batches in training
+LR = 3e-4               # the learning rate of the optimizer: in our case AdamW
+EVAL_INTERVAL = 50      # every after those epochs we are evaluating the loss of the model
+EVAL_ITERS = 5          # for how many batches we should calculate the loss when evaluating the model
+NUM_HEADS = 6           # the number of Masked Self-Attention layers
+NUM_LAYERS = 6          # the number of Block Layer (each Block contains one Mulit-Head Attention and one Feed-Forward layer)
+DROPOUT = 0.2           # the dropout we are setting in the layers
+SCALE_EMBEDS = 4        # how many times we want to scale the embedding dimension in the Feed-Froward layer
 
+
+
+### Setting Devide Agnostic Code
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
 
 ### Loading the Dataset into DataFrame Format
 df = pd.read_csv("sms_spam.csv", encoding="latin-1")[["v1", "v2"]] # By default there are another 2 columns that doe't have data
@@ -40,43 +103,15 @@ max_sentence_length = df["raw_text"].str.len().max()
 df["label"] = df["num_label"].map({"ham": [1, 0], "spam": [0, 1]})
 df["proc_text"] = df["raw_text"].apply(lambda l: "{" + l + "".join(["}" for _ in range(max_sentence_length - len(l))]))
 df["tokenized_text"] = df["proc_text"].apply(encoder)
-
 max_sentence_length += 1 # Due to the [CLC] token
+
+
 
 ### Splitting the Dataset into Training and Validation Sets
 train_size = int(len(df) * 0.9)
 train_ds, valid_ds = df.iloc[:train_size], df.iloc[train_size:].reset_index(drop=True) # don't add the previous indexes as a new column
 
-### Creating the Function that Splits the Dataset into Batches
-def get_batch(split=None):
-    data = train_ds if split == "train" else valid_ds
 
-    idx_xb = torch.randint(len(data), (BATCH_SIZE,)) # Random indexes from the Dataset
-
-    x = torch.stack([torch.tensor(data["tokenized_text"][i.item()], dtype=torch.long) for i in idx_xb])
-    y = torch.tensor([data["label"][i.item()] for i in idx_xb], dtype=torch.long)    
-
-    return x.to(device=device, non_blocking=True), y.to(device=device, non_blocking=True)
-
-### Creating a Loss Calculation Function
-@torch.inference_mode()
-def estimate_loss(model):
-    out = {}
-
-    model.eval()
-    for split in ["train", "valid"]:
-        losses = torch.zeros(EVAL_ITERS)
-
-        # Iterate `EVAL_ITERS` number of times to reduce the noice that each batch contains
-        for i in range(EVAL_ITERS):
-            xb, yb = get_batch(split)
-            _, loss = model(xb, yb)
-            losses[i] = loss.item()
-        
-        out[split] = losses.mean()
-    
-    model.train()
-    return out
 
 ### Initializing the Model
 model = TransformerEncoder(
@@ -91,8 +126,12 @@ model = TransformerEncoder(
     device = device
 ).to(device)
 
+
+
 ### Setting Optimizer
 optimizer = torch.optim.AdamW(model.parameters(), lr=LR)
+
+
 
 ### Training the Model
 print("\nStarting Training:")
@@ -111,6 +150,8 @@ for epoch in tqdm(range(1, EPOCHS+1)):
     loss.backward()
     optimizer.step()
 
+
+
 ### Making Predictions
 xb, yb = get_batch()
 
@@ -118,6 +159,6 @@ for i in range(BATCH_SIZE):
     x_first_sample, y_first_sample = xb[i], yb[i]
 
     print(decoder(x_first_sample.tolist()).split('}')[0][1:])
-    print(model.predict(x_first_sample))
+    print(predict(model, x_first_sample))
     print(y_first_sample.tolist())
     print()
